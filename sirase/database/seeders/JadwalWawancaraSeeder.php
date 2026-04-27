f70701790504cc651a6516b6a4e6ad9c92a03e6e
Audry Verly <audry.verly@gmail.com>
2026-04-28 00:35:39 +0700

    seeder sejauh ini udah keisis (kurang 7 seeder lagi)

--- sirase/database/seeders/JadwalWawancaraSeeder.php
+++ sirase/database/seeders/JadwalWawancaraSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class JadwalWawancaraSeeder extends Seeder
 {
@@ -12,6 +12,33 @@ class JadwalWawancaraSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('jadwal_wawancara')->insert([
+            [
+                'idProgressTahapan' => 12,
+                'idPendaftaran' => 1,
+                'tanggal_wawancara' => '2025-02-04',
+                'waktu_mulai' => '15:00:00',
+                'waktu_selesai' => '16:00:00',
+                'lokasi' => 'BA 06.01',
+                'link_wawancara' => '',
+                'keterangan' => 'Silahkan datang tepat waktu',
+                'status' => 'selesai',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'idProgressTahapan' => 16,
+                'idPendaftaran' => 2,
+                'tanggal_wawancara' => '2026-03-06',
+                'waktu_mulai' => '12:00:00',
+                'waktu_selesai' => '13:00:00',
+                'lokasi' => 'TS 01.01',
+                'link_wawancara' => '',
+                'keterangan' => 'Datang tepat waktu',
+                'status' => 'selesai',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
--- sirase/database/seeders/WawancaraPenilaiSeeder.php
+++ sirase/database/seeders/WawancaraPenilaiSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class WawancaraPenilaiSeeder extends Seeder
 {
@@ -12,6 +12,39 @@ class WawancaraPenilaiSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('wawancara_penilai')->insert([
+            [
+                'idJadwalWawancara' => 1,
+                'idStaffUnit' => 7,
+                'status' => 'sudah',
+                'token' => '8s2oWFtfQeN255S6xEziSAROZLyfT8xyYtPw5AIg',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'idJadwalWawancara' => 1,
+                'idStaffUnit' => 8,
+                'status' => 'sudah',
+                'token' => 'dVa7PtwbZJfn5saV38oGBdaBPSkinWk0l2Nx34BO',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'idJadwalWawancara' => 2,
+                'idStaffUnit' => 9,
+                'status' => 'sudah',
+                'token' => 'Ox4bmdKGx44RSB5HejrXWCAmicrdF5Xl9oQTqi6d',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'idJadwalWawancara' => 2,
+                'idStaffUnit' => 10,
+                'status' => 'sudah',
+                'token' => 'EaZ7don1omZ0ByrkjcHtBQwN2Pv7uti7zXBKuWxj',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
